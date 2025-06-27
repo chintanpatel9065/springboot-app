e3105638d5f8ec2ffbe528e1260594d263dbfc29
chintanpatel <chintanpatel9065@gmail.com>
2025-06-27 13:03:16 +0530

    Add DepartmentController to handle department-related web requests

---
+++ src/main/java/org/chintanpatel/springbootapp/department/DepartmentController.java
@@ -0,0 +1,61 @@
+package org.chintanpatel.springbootapp.department;
+
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class DepartmentController {
+
+    private final DepartmentService departmentService;
+
+    public DepartmentController(DepartmentService departmentService) {
+        this.departmentService = departmentService;
+    }
+
+    @GetMapping("/departments/listDepartment")
+    public String listDepartment(Model model){
+        List<Department> departmentList = departmentService.getAllDepartmentList();
+        model.addAttribute("departmentList", departmentList);
+        return "department/department-list";
+    }
+
+    @GetMapping("/departments/showDepartmentForm")
+    public String showDepartmentForm(Model model){
+        Department department = new Department();
+        model.addAttribute("department", department);
+        return "department/department-form";
+    }
+
+    @PostMapping("/departments/insertOrUpdateDepartment")
+    public String insertOrUpdateDepartment(@Valid @ModelAttribute("department") Department department, BindingResult bindingResult, Model model){
+        if(bindingResult.hasErrors()){
+            return "department/department-form";
+        }
+        departmentService.addDepartment(department);
+        return "redirect:/departments/listDepartment";
+    }
+
+    @GetMapping("/departments/manageDepartment/{departmentId}")
+    public String manageDepartment(@PathVariable Long departmentId, Model model){
+        if (departmentId != null){
+            Department department = departmentService.getDepartmentById(departmentId);
+            model.addAttribute("department", department);
+        }
+        return "department/department-form";
+    }
+
+    public  String deleteDepartment(@PathVariable Long departmentId){
+        if (departmentId != null){
+            departmentService.deleteDepartmentById(departmentId);
+        }
+        return "redirect:/departments/listDepartment";
+    }
+}
