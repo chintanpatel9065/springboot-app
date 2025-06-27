7dbe832fff76339b55a9be0cef5a3c78ed072beb
chintanpatel <chintanpatel9065@gmail.com>
2025-06-27 17:25:04 +0530

    Add EmployeeRepository interface for database operations

---
+++ src/main/java/org/chintanpatel/springbootapp/employee/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package org.chintanpatel.springbootapp.employee;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("employeeRepository")
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
